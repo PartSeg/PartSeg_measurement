1a5354de34208c33c54103aa10c4cd19ad26cd55
Grzegorz Bokota <bokota+github@gmail.com>
2022-05-31 19:41:30 +0200

    add initial per_component tests

--- src/PartSeg_measurement/_tests/test_measurement.py
+++ src/PartSeg_measurement/_tests/test_measurement.py
@@ -18,16 +18,18 @@ def units():
 
 @pytest.fixture
 def image():
-    data = np.zeros((10, 10, 10))
+    data = np.zeros((10, 10, 20))
     data[2:-2, 2:-2, 2:-2] = 10
     data[3:-3, 3:-3, 3:-3] = 20
-    data[4:-4, 4:-4, 4:-4] = 30
+    data[4:-4, 4:-4, 4:-14] = 30
     return data
 
 
 @pytest.fixture
 def labels(image):
-    return (image > 15).astype(np.uint8)
+    res = (image > 15).astype(np.uint8)
+    res[3:-3, 3:-3, 10:-3] = 2
+    return res
 
 
 def mask(image):
@@ -39,15 +41,34 @@ class TestVolume:
         assert isinstance(volume, MeasurementFunctionWrap)
 
     def test_call(self, labels, units):
-        assert volume(labels=labels, voxel_size=(1, 1, 1)) == 64
-        assert volume(labels=labels, voxel_size=(2, 1, 1)) == 128
+        assert volume(labels=labels, voxel_size=(1, 1, 1)) == 224
+        assert volume(labels=labels, voxel_size=(2, 1, 1)) == 448
         assert (
             volume(
                 labels=labels,
                 voxel_size=(2 * units.nm, 1 * units.nm, 1 * units.nm),
             )
-            == 128 * units.nm**3
+            == 448 * units.nm**3
         )
+        assert (
+            volume(
+                labels=labels,
+                voxel_size=np.array([2, 1, 1]) * units.nm,
+            )
+            == 448 * units.nm**3
+        )
+
+    def test_per_component_call(self, labels, units):
+        assert list(
+            volume(labels=labels, voxel_size=(1, 1, 1), per_component=True)
+        ) == [112, 112]
+        assert list(
+            volume(
+                labels=labels,
+                voxel_size=np.array((2, 2, 1)) * units.nm,
+                per_component=True,
+            )
+        ) == [448 * units.nm**3, 448 * units.nm**3]
 
 
 class TestVoxels:
@@ -55,7 +76,13 @@ class TestVoxels:
         assert isinstance(voxels, MeasurementFunctionWrap)
 
     def test_call(self, labels, units):
-        assert voxels(labels=labels) == 64 * units.pixel
+        assert voxels(labels=labels) == 224 * units.pixel
+
+    def test_call_per_component(self, labels, units):
+        assert list(voxels(labels=labels, per_component=True)) == [
+            112 * units.pixel,
+            112 * units.pixel,
+        ]
 
 
 class TestPixelBrightness:
